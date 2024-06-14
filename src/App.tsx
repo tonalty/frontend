40855eecd0d56cfa2f48a3604ae456ceb81f440e
Konstantin Zharich <kostantin_zharich@epam.com>
2024-06-14 09:19:38 +0200

    - returned back

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { ConnectCommunity } from "./pages/ConnectCommunity";
 import { ConnectBot } from "./pages/ConnectBot";
 import { GlobalStyles } from "./components/GlobalStyle";
 import { darkTheme, lightTheme } from "./components/Theme";
-import { Typography } from "@mui/material";
 
 const router = createBrowserRouter([
   {
@@ -47,21 +46,18 @@ function App() {
 // https://habr.com/ru/articles/666278/
   const isDarkMode = window.Telegram.WebApp.colorScheme === 'dark';
 
-  
-  console.log('window.Telegram.WebApp.initData', window.Telegram.WebApp.initData)
-  console.log('start_param', window.Telegram.WebApp.initDataUnsafe.start_param);
+  console.log('start_param', window.Telegram.WebApp.initDataUnsafe?.start_param);
 
   console.log('isDarkMode', isDarkMode);
   console.log(`Network: `, network ? network === CHAIN.MAINNET ? "mainnet" : "testnet": "N/A");
 
   return (
     <>
-      start_param {window.Telegram.WebApp.initDataUnsafe.start_param}
+      start_param {window.Telegram.WebApp.initDataUnsafe?.start_param}
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <GlobalStyles/>
         <RouterProvider router={router} />
         {/* <Typography>{ JSON.stringify(window.location.href) }</Typography> */}
-        <Typography>{ JSON.stringify(window.Telegram.WebApp.initData) }</Typography>
       </ThemeProvider>
     </>
   );
