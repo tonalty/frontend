a5a23f0f106afe37290cee0e045d430ec57dbfca
Alexey Elizarov <felex-ae@ya.ru>
2024-06-25 23:44:25 +0300

    chore: improve code

--- src/components/Theme.ts
+++ src/components/Theme.ts
@@ -1,14 +1,14 @@
+// import { createTheme } from '@mui/materiali'
+
 export const lightTheme = {
   body: '#FFF',
   text: '#363537',
-  bgCommunityListItem: 'linear-gradient(-225deg, #f6f6f67d 0%, #efefef96 100%)'
-};
+}
 
 export const darkTheme = {
   body: '#363537',
   text: '#FAFAFA',
-  bgCommunityListItem: 'linear-gradient(-225deg, #f6f6f62b 0%, #efefef96 100%)'
-};
+}
 
 // // TODO Rewrite default inputs styles
 // export const muiGlobalOverrides = createTheme({
