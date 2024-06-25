515791a95e433db5f16b859890579a14afa6c5a7
Alexey Elizarov <felex-ae@ya.ru>
2024-06-25 23:22:44 +0300

    feat: eslint imports sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,37 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  Object.assign({}, pluginReactConfig, { rules: { 'react/react-in-jsx-scope': 0 } })
+  Object.assign({}, pluginReactConfig, {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      import: importPlugin
+    },
+    rules: {
+      'react/react-in-jsx-scope': 0,
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'], // bare imports,
+            ['^react', '^'], // react, non-local imports
+            ['^@/', '^\\.'] // local imports
+          ]
+        }
+      ],
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error'
+    }
+  })
 ];
