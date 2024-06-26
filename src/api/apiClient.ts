1c5e5fd6d77b3e41645e886236a5c36e8b833a94
Alexey Elizarov <felex-ae@ya.ru>
2024-06-26 10:53:38 +0300

    feat: openapi with part of hooks

---
+++ src/api/apiClient.ts
@@ -0,0 +1,31 @@
+import { retrieveLaunchParams } from '@tma.js/sdk';
+import createClient, { Middleware } from 'openapi-fetch';
+
+import type { paths } from './schema';
+
+const apiClient = createClient<paths>({
+  baseUrl: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    'Cache-Control': 'no-store, must-revalidate',
+    Pragma: 'no-cache',
+    Expires: '0'
+  }
+});
+
+const myMiddleware: Middleware = {
+  async onRequest({ request }) {
+    try {
+      const { initDataRaw } = retrieveLaunchParams();
+
+      request.headers.set('tmaInitData', initDataRaw!);
+    } catch (error) {
+      // Browser probably
+    }
+
+    return request;
+  }
+};
+
+apiClient.use(myMiddleware);
+
+export { apiClient };
--- src/pages/RewardShop.tsx
+++ src/pages/RewardShop.tsx
@@ -1,16 +1,14 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
-import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
 import { Avatar, Button, LargeTitle, Text } from '@telegram-apps/telegram-ui';
 import { useTonWallet } from '@tonconnect/ui-react';
-import axios from 'axios';
 
+import { useUserCommunity } from '@/api/queries';
 import { ConnectWalletWithPlaceholder } from '@/components/ConnectWalletWithPlaceholder';
 import HistoryTable from '../components/HistoryTable';
 import { ProductSlider } from '../components/ProductSlider';
-import { CommunityUser } from '../interfaces/CommunityUser';
 import { EarnPoints } from './EarnPoints';
 
 interface Props {
@@ -19,21 +17,9 @@ interface Props {
 
 export function RewardShop(props: Props) {
   const { id } = useParams();
-  const [userCommunity, setUserCommunity] = useState<CommunityUser | null>(null);
-
-  async function getUserCommunity() {
-    const result = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/communities/${id}`, {
-      headers: { tmaInitData: window.Telegram.WebApp.initData }
-    });
-
-    setUserCommunity(result.data);
-  }
+  const { data: userCommunity } = useUserCommunity(id);
 
   const wallet = useTonWallet();
-
-  useEffect(() => {
-    getUserCommunity();
-  }, []);
 
   if (!userCommunity) {
     return <span>No user commmunity</span>;
