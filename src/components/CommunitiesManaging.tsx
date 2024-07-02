8ce9173f310b8b5978ec9bb5ae0385813b871833
Konstantin Zharich <kostantin_zharich@epam.com>
2024-07-02 18:57:54 +0200

    not fixed

--- src/components/CommunitiesManaging.tsx
+++ src/components/CommunitiesManaging.tsx
@@ -1,24 +1,21 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Avatar, Button, Cell, FixedLayout, Text } from '@telegram-apps/telegram-ui';
+import { classNames, useMiniApp } from '@tma.js/sdk-react';
 
 import { PlusCircleIcon } from '@/icons/PlusCircleIcon';
 import { CommunityUser } from '@/interfaces/CommunityUser';
+import styles from './Item.module.css';
 import { ModalNewCommunity } from './modals/ModalNewCommunity/ModalNewCommunity';
 import { NoData } from './NoData';
-
-const CellStyles = {
-  height: '68px',
-  marginTop: '32px',
-  background: 'inherit',
-  borderRadius: '30px'
-};
 
 interface Props {
   community: CommunityUser[];
 }
 
 export const CommunitiesManaging: FC<Props> = ({ community }) => {
+  const miniApp = useMiniApp();
+
   return (
     <div style={{ width: '100%', paddingBottom: '80px' }}>
       {community.length ? (
@@ -33,7 +30,10 @@ export const CommunitiesManaging: FC<Props> = ({ community }) => {
               key={index}
               style={{ color: 'inherit', textDecoration: 'none', background: 'inherit' }}>
               <Cell
-                style={CellStyles}
+                className={classNames(
+                  miniApp.isDark ? styles.whiteText : styles.darkText,
+                  styles.cell
+                )}
                 subtitle={`Earned points: ${community.points}`}
                 before={<Avatar src={avatarSrc} />}>
                 {community?.communityName}
--- src/components/CommunitiesSubscribed.tsx
+++ src/components/CommunitiesSubscribed.tsx
@@ -1,16 +1,11 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Avatar, Cell, Text } from '@telegram-apps/telegram-ui';
+import { classNames } from '@tma.js/sdk-react';
 
 import { CommunityUser } from '@/interfaces/CommunityUser';
+import styles from './Item.module.css';
 import { NoData } from './NoData';
-
-const CellStyles = {
-  height: '68px',
-  marginTop: '32px',
-  background: 'inherit',
-  borderRadius: '30px'
-};
 
 interface Props {
   community: CommunityUser[];
@@ -31,7 +26,7 @@ export const CommunitiesSubscribed: FC<Props> = ({ community }) => {
               state={{ avatarSrc }}
               style={{ color: 'inherit', textDecoration: 'none', background: 'inherit' }}>
               <Cell
-                style={CellStyles}
+                className={classNames(styles.cell)}
                 subtitle={`Earned points: ${community.points}`}
                 before={<Avatar src={avatarSrc} />}>
                 {community?.communityName}
