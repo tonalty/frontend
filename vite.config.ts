89ad761ee3a03858f58c2690a4c31658fe6ddd79
Lex <lex.lar@gmail.com>
2024-07-02 21:45:08 +0300

    Updated vite config to support CD.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { UserConfig, defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  base: ((process.env.GITHUB_REPOSITORY ?? '') + '/').match(/(\/.*)/)?.[1],
-  server: {
-    https: {
-      key: fs.readFileSync('../localhost.direct.key'),
-      cert: fs.readFileSync('../localhost.direct.crt')
-    },
-    host: 'tonalty.localhost.direct'
-  },
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    plugins: [react(), nodePolyfills()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+    }
+  };
+  if (mode === 'development') {
+    config.server = {
+      https: {
+        key: fs.readFileSync('../localhost.direct.key'),
+        cert: fs.readFileSync('../localhost.direct.crt')
+      },
+      host: 'tonalty.localhost.direct'
+    };
   }
+  return config;
 });
