17bfb0fe254177b27f4d59ebdd7ba7790d1bb4ae
konstantinzharich <kostyasupikos@gmail.com>
2024-07-04 00:53:48 +0200

    Added item to history section (#25)
    * - temp
    
    * - changed value
    
    ---------
    
    Co-authored-by: Konstantin Zharich <kostantin_zharich@epam.com>

--- src/enums/TriggerType.ts
+++ src/enums/TriggerType.ts
@@ -1,4 +1,5 @@
 export enum TriggerType {
   messageReaction = 'messageReaction',
-  referralJoin = 'referralJoin'
+  referralJoin = 'referralJoin',
+  rewardBuy = 'rewardBuy'
 }
--- src/utils/common.tsx
+++ src/utils/common.tsx
@@ -2,15 +2,21 @@ import { TriggerType } from '@/enums/TriggerType';
 import { ReactionIcon } from '@/icons/ReactionIcon';
 import { ReferralIcon } from '@/icons/ReferralIcon';
 
-export const getIcon = (type: string, isInvertedColor: boolean = false) => {
+export const getIcon = (type: string, isInvertedColor: boolean = false, src?: string) => {
   if (type === TriggerType.messageReaction) {
     return <ReactionIcon isInvertedColor={isInvertedColor} />;
   } else if (type === TriggerType.referralJoin) {
     return <ReferralIcon isInvertedColor={isInvertedColor} />;
+  } else if (type === TriggerType.rewardBuy) {
+    return <img style={{ borderRadius: 100 }} src={src} width={48} height={48}></img>;
   }
 };
 
 export const formatPoints = (points: number) => {
+  if (points.toString()[0] === '-') {
+    return `${points.toFixed(2)}`;
+  }
+
   const sign = points > 0 ? '+' : '-';
 
   return `${sign}${points.toFixed(2)}`;
